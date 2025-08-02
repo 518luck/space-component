1aea540a3c7f27f0d4c923ca5935c6ff13780ee1
蜻蜓ink队长 <165388484+518luck@users.noreply.github.com>
2025-08-02 15:57:11 +0800

    feat:类型定义

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,12 @@
-import { Space } from 'antd'
+import Space from './Space'
 
 import './App.css'
+
 function App() {
   return (
     <div>
-      <Space direction='vertical'>
-        <div className='box'></div>
-        <div className='box'></div>
-        <div className='box'></div>
-      </Space>
+      <Space></Space>
     </div>
   )
 }
-
 export default App
---
+++ src/Space/index.tsx
@@ -0,0 +1,18 @@
+export type SizeType = 'small' | 'middle' | 'large' | number | undefined
+export interface SpaceProps extends React.HTMLAttributes<HTMLDivElement> {
+  className?: string
+  style?: React.CSSProperties
+  size?: SizeType | [SizeType, SizeType]
+  direction?: 'horizontal' | 'vertical'
+  align?: 'start' | 'end' | 'center' | 'baseline'
+  split?: React.ReactNode
+  wrap?: boolean
+}
+
+const Space: React.FC<SpaceProps> = (props) => {
+  const { className, style, ...otherProps } = props
+
+  return <div className={className} style={style} {...otherProps}></div>
+}
+
+export default Space
